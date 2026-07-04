aafb4e693c6004497645d7a41c4c87c42aed8692
XIao Chen <xiaochen@JasondeMac-mini.local>
2026-07-04 13:42:43 +0800

    📜 Migrate latest old blog post (RTL8168 driver) + add About page update
    - src/pages/about.astro: Add '前身' section bridging myinxd.github.io era
    - src/content/blog/2019-07-27-...mdx: Migrated last article from old blog
      with retro-style intro and modern commentary on engineering habits

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
+["src/content/blog/2019-07-27-configure-rtl8168-ethernet-card-on-ubuntu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2019-07-27-configure-rtl8168-ethernet-card-on-ubuntu.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hello-pulsar-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-pulsar-guide.mdx&astroContentModuleFlag=true")],
 ["src/content/paper-notes/bevformer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpaper-notes%2Fbevformer.mdx&astroContentModuleFlag=true")]]);
 		
